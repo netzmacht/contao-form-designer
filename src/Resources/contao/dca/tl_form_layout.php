18611dbe86ffc6c22875f700653ddbc48d45056c
David Molineus <david.molineus@netzmacht.de>
2017-09-07 15:32:37 +0200

    Fix theme name field.

--- src/Resources/contao/dca/tl_form_layout.php
+++ src/Resources/contao/dca/tl_form_layout.php
@@ -95,7 +95,7 @@ $GLOBALS['TL_DCA']['tl_form_layout'] = [
             'sql'    => "int(10) unsigned NOT NULL auto_increment",
         ],
         'pid'             => [
-            'foreignKey' => 'tl_theme.title',
+            'foreignKey' => 'tl_theme.name',
             'sql'        => "int(10) unsigned NOT NULL default '0'",
             'relation'   => ['type' => 'belongsTo', 'load' => 'lazy'],
         ],
