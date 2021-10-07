f1fb4e7b43bb4286e6a799def0e93f3f7a354167
David Molineus <david.molineus@netzmacht.de>
2021-10-07 11:57:28 +0200

    Allow customizing bundle configuration

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,20 +14,32 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Netzmacht\Contao\FormDesigner\NetzmachtContaoFormDesignerBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface
 {
     /**
      * {@inheritdoc}
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(NetzmachtContaoFormDesignerBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
+    {
+        $loader->load(__DIR__ . '/../Resources/config/config.yml');
     }
 }
--- src/DependencyInjection/NetzmachtContaoFormDesignerExtension.php
+++ src/DependencyInjection/NetzmachtContaoFormDesignerExtension.php
@@ -20,14 +20,23 @@ class NetzmachtContaoFormDesignerExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
 
-        $loader->load('config.yml');
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $container->setParameter('netzmacht.contao_form_designer.widgets', $config['widgets']);
+        $container->setParameter('netzmacht.contao_form_designer.form_designer.elements', $config['elements']);
+        $container->setParameter('netzmacht.contao_form_designer.form_designer.modules', $config['modules']);
+        $container->setParameter(
+            'netzmacht.contao_form_designer.form_designer.virtual_widgets',
+            $config['virtual_widgets']
+        );
+
         $loader->load('services.yml');
         $loader->load('listeners.yml');
     }
