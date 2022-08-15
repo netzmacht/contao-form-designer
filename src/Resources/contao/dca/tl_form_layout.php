bb931ad6664a2b80b4327432533897cfd49854cf
David Molineus <david.molineus@netzmacht.de>
2022-08-15 11:21:39 +0200

    Prevent undefined array key index warning
    Close #28 as we stick with the solution used in Contao

--- src/Resources/contao/dca/tl_form_layout.php
+++ src/Resources/contao/dca/tl_form_layout.php
@@ -75,7 +75,7 @@ $GLOBALS['TL_DCA']['tl_form_layout'] = [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.svg',
                 'attributes' => 'onclick="if(!confirm(\''
-                    . $GLOBALS['TL_LANG']['MSC']['deleteConfirm']
+                    . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '')
                     . '\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
